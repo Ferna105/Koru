00b1dc223588c4f9f6facabc621c1f53df197502
Fernando Mariscotti <fernando.mariscotti@ripio.com>
2024-02-25 20:48:45 -0300

    configuración de librerias

--- src/screens/private/homeTabs/tabs/matchTabs/matchTabs.screen.tsx
+++ src/screens/private/homeTabs/tabs/matchTabs/matchTabs.screen.tsx
@@ -3,16 +3,15 @@ import React from 'react';
 import { HomeTabMatchTabParamList, HomeTabScreenProps } from 'navigation/types';
 import { SearchMatch } from './tabs/searchMatch/searchMatch.screen';
 import { CreateMatch } from './tabs/createMatch/createMatch.screen';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const TopTabNavigator =
-  createMaterialTopTabNavigator<HomeTabMatchTabParamList>();
+const BottomTabNavigator = createBottomTabNavigator<HomeTabMatchTabParamList>();
 
 export const MatchTabs = ({}: HomeTabScreenProps<'MatchTabs'>) => {
   return (
-    <TopTabNavigator.Navigator>
-      <TopTabNavigator.Screen name="CreateMatch" component={CreateMatch} />
-      <TopTabNavigator.Screen name="SearchMatch" component={SearchMatch} />
-    </TopTabNavigator.Navigator>
+    <BottomTabNavigator.Navigator>
+      <BottomTabNavigator.Screen name="CreateMatch" component={CreateMatch} />
+      <BottomTabNavigator.Screen name="SearchMatch" component={SearchMatch} />
+    </BottomTabNavigator.Navigator>
   );
 };
